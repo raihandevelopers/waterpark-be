9896f3c29279d793d60c58d3bb036ab7492c4a01
raihandevelopers <raihankhan.indore@gmail.com>
2024-12-11 20:15:32 +0530

    Changes Banner

--- controllers/bannerController.js
+++ controllers/bannerController.js
@@ -1,6 +1,7 @@
 const Banner = require('../models/banner');
 const path = require('path');
 
+
 // Controller to upload a new banner image
 exports.uploadBanner = async (req, res) => {
   try {
@@ -37,7 +38,10 @@ exports.getBanner = async (req, res) => {
       return res.status(404).json({ message: 'No banner found' });
     }
 
-    res.status(200).json({ banner });
+    // Construct the full URL for the image
+    const bannerImageUrl = `${BASE_URL}/${banner.image.replace(/\\/g, '/')}`;
+
+    res.status(200).json({ banner: { ...banner.toObject(), image: bannerImageUrl } });
   } catch (error) {
     console.error('Error fetching banner:', error);
     res.status(500).json({ message: 'Internal server error', error: error.message });
