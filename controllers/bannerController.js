266eef56d16237a51671b5b417cfaf71d4bcb104
raihandevelopers <raihankhan.indore@gmail.com>
2024-12-13 18:22:18 +0530

    Changes

--- controllers/bannerController.js
+++ controllers/bannerController.js
@@ -29,18 +29,15 @@ exports.uploadBanner = async (req, res) => {
 // Controller to get the current banner
 exports.getBanner = async (req, res) => {
   try {
-    const banner = await Banner.find({}); // Fetch all banners
-
-    if (!banner) {
-      return res.status(404).json({ message: 'No banner found' });
-    }
-
-    // Construct the full URL for the image
-    const bannerImageUrl = `${BASE_URL}/${banner.image.replace(/\\/g, '/')}`;
-
-    res.status(200).json({ banner: { ...banner.toObject(), image: bannerImageUrl } });
+    const banners = await Banner.find(); // Retrieve all documents in the collection
+    const updatedBanners = banners.map((banner) => ({
+      ...banner.toObject(),
+      image: `${BASE_URL}/${banner.image.replace(/\\/g, '/')}`,
+    }));
+    res.status(200).json({ banners: updatedBanners });
   } catch (error) {
-    console.error('Error fetching banner:', error);
+    console.error('Error fetching banners:', error);
     res.status(500).json({ message: 'Internal server error', error: error.message });
   }
 };
+
